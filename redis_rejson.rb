af4aeeddfd69973e9536860301d532f5a15c5ecd
makevoid <makevoid@gmail.com>
2017-05-17 22:11:45 +0100

    implemented base api , key/value api, strings, nums... - status ~45%

--- redis_rejson.rb
+++ redis_rejson.rb
@@ -5,12 +5,97 @@ class Redis
   module ReJSON
   end
 
-  def json_set(key, value)
-    client.call ["JSON.SET", key,  ".", value]
+  # key
+
+  def json_set(key, value, p: nil)
+    client.call json_cmd_two(:set, key, value, p)
   end
 
-  def json_get(key)
-    client.call ["JSON.GET", key]
+  def json_get(key, p: nil)
+    client.call json_cmd_one(:get, key, p)
+  end
+
+  def json_type(key, p: nil)
+    client.call json_cmd_one(:type, key, p)
+  end
+
+  def json_mget(key, p: nil)
+
+  end
+
+  # string
+
+  def json_str_len(key, p: nil)
+    client.call json_cmd_one(:strlen, key, p)
+  end
+
+  def json_str_append(key, value, p: nil)
+    client.call json_cmd_two(:strappend, key, value, p)
+  end
+
+  # num
+
+  def json_num_incr_by(key, value, p: nil)
+    jto_i client.call json_cmd_two(:numincrby, key, value, p)
+  end
+
+  def json_num_incr(key, p: nil)
+    jto_i client.call json_cmd_two(:numincrby, key, 1, p)
+  end
+
+  def json_num_mult_by(key, value, p: nil)
+    jto_i client.call json_cmd_two(:nummultby, key, value, p)
+  end
+
+  def json_num_mult_double(key, p: nil)
+    jto_i client.call json_cmd_two(:nummultby, key, 2, p)
+  end
+
+
+  # array
+
+  def json_arr_append(key, value, p: nil)
+    jto_i client.call json_cmd_two(:arrappend, key, value, p)
+  end
+
+  def json_arr_index(key, value, p: nil)
+    jto_i client.call json_cmd_two(:arrindex, key, value, p)
+  end
+
+  def json_arr_insert(key, value, p: nil)
+    jto_i client.call json_cmd_two(:arrinsert, key, value, p)
+  end
+
+  def json_arr_len(key, value, p: nil)
+    jto_i client.call json_cmd_two(:arrlen, key, value, p)
+  end
+
+  # object
+
+  # util
+
+  private
+
+  def jto_i(value)
+    value.to_i
+  end
+
+  # json command, one parameter
+  def json_cmd_one(cmd, key, path)
+    unless path
+      ["JSON.#{cmd.upcase}", key]
+    else
+      ["JSON.#{cmd.upcase}", key, path]
+    end
+  end
+
+  # json command, two parameters
+  def json_cmd_two(cmd, key, value, path)
+    unless path
+      ["JSON.#{cmd.upcase}", key, ".", value]
+    else
+      ["JSON.#{cmd.upcase}", key, path, value]
+    end
   end
 
 end
--- spec/redis_rejson_spec.rb
+++ spec/redis_rejson_spec.rb
@@ -8,18 +8,96 @@ describe "RedisReJSON" do
     R.flushdb
   end
 
+  # keys
+
   it "gets a blank value" do
     foo = R.json_get "foo"
     foo.should be_nil
   end
 
-  it "sets blank value" do
-    foo = R.json_set "foo", "\"bar\""
-    foo.should eq "OK"
+  it "sets a value" do
+    set = R.json_set "foo", '"bar"'
+    set.should eq "OK"
   end
 
   it "gets a value" do
     foo = R.json_get "foo"
-    foo.should eq "\"bar\""
+    foo.should eq '"bar"'
   end
+
+  it "gets a value (w/ p)" do
+    foo = R.json_get "foo", p: "."
+    foo.should eq '"bar"'
+  end
+
+  it "gets the type" do
+    type = R.json_type "foo"
+    type.should eq "string"
+  end
+
+  it "gets the length of a string" do
+    foo = R.json_str_len "foo"
+    foo.should eq 3
+  end
+
+  # string
+
+  it "appends to a string" do
+    foo = R.json_str_append "foo", '"baz"'
+    foo.should eq 6
+    foo = R.json_get "foo"
+    foo.should eq '"barbaz"'
+  end
+
+  it "sets a value (w/ path)" do
+    set = R.json_set "foo", '{"a": "b"}', p: "."
+    set.should eq "OK"
+    set = R.json_set "foo", '"bar"', p: "a"
+    set.should eq "OK"
+    foo = R.json_get "foo", p: "a"
+    foo.should eq '"bar"'
+  end
+
+  it "gets the length of a string" do
+    len = R.json_str_len "foo"
+    len = R.json_str_len "foo", p: "a"
+
+  end
+
+  # num
+
+  it "increments a num" do
+    set = R.json_set "num", '0'
+    set.should eq "OK"
+    num = R.json_num_incr_by "num", 1
+    num.should eq 1
+    num = R.json_num_incr_by "num", 2
+    num.should eq 3
+  end
+
+  it "increments a num (numincr 1 - easier api)" do
+    num = R.json_num_incr "num"
+    num.should eq 4
+  end
+
+  it "multiplies a num by x" do
+    num = R.json_num_mult_by "num", 2
+    num.should eq 8
+  end
+
+  it "multiplies a num (double)" do
+    num = R.json_num_mult_by "num", 2
+    num.should eq 16
+  end
+
+  # arrays
+
+  # TODO
+
+  # objects
+
+  # utils
+
+
+
 end
